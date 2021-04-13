1097d32389cb800ac579f447d4ded2cf6a7944b9
David Moškoř <mozkomor05@gmail.com>
2021-04-13 14:12:20 +0200

    v0.0.5

--- YTARB.user.js
+++ YTARB.user.js
@@ -1,140 +1,145 @@
 // ==UserScript==
-// @name         YouTube Age Restriction Bypass
-// @namespace    https://github.com/mozkomor05/YT-Age-Restriction-Bypass 
-// @version      0.0.1
-// @description  can't stop me from listening to S3RL - Hentai
-// @include      https://www.youtube.com/*
-// @connect      googlevideo.com
-// @grant        GM.xmlHttpRequest
-// @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @run-at       document-start
+// @name             YouTube Age Restriction Bypass
+// @namespace       https://github.com/mozkomor05/YT-Age-Restriction-Bypass 
+// @version         0.0.5
+// @description     can't stop me from listening to S3RL - Hentai
+// @author          mozkomor05 (https://github.com/mozkomor05)
+// @namespace       https://github.com/mozkomor05/YT-Age-Restriction-Bypass/
+// @updateURL       https://github.com/mozkomor05/YT-Age-Restriction-Bypass/raw/main/YTARB.user.js
+// @downloadURL     https://github.com/mozkomor05/YT-Age-Restriction-Bypass/raw/main/YTARB.user.js
+// @supportURL      https://github.com/mozkomor05/YT-Age-Restriction-Bypass/issues
+// @include         https://www.youtube.com/*
+// @connect         googlevideo.com
+// @require         https://code.jquery.com/jquery-3.6.0.min.js
+// @run-at          document-start
 // ==/UserScript==
 
 (function () {
-  'use strict'
+    'use strict'
 
-  if (!('unsafeWindow' in window))
-    return;
-
-  function init() {
-    if (location.pathname.startsWith('/embed/'))
-      bypassEmbed();
-    else {
-      addEventListener('yt-navigate-start', resetPage);
-      addEventListener('yt-navigate-finish', bypassPage);
-    }
-  }
-
-  function getVideoId() {
-    return (location.pathname.match(/^\/embed\/([a-zA-Z0-9_-]+)$/) || [])[1] || new URLSearchParams(location.search).get('v');
-  }
-
-  function resetPage() {
-    $('#bypassed-player').remove();
-    $('#ytd-player').show();
-    $('#error-screen').show();
-  }
-
-  function waitForErrScreen(errScreen) {
-    return new Promise((resolve, reject) => {
-      const exists = setInterval(function () {
-        if (errScreen.html()) {
-          clearInterval(exists);
-          resolve(errScreen);
-        }
-      }, 100);
-    })
-  }
-
-  function bypassPage() {
-    waitForErrScreen($('#error-screen')).then(function ($errorScreen) {
-      let restricted = false;
-      
-      if ($errorScreen.is(":visible"))
-        $errorScreen.find("a.yt-simple-endpoint").each(function () {
-          if (this.href.includes('watching_arv')) {
-            restricted = true;
-            return;
-          }
-        });
-
-      if (!restricted)
+    if (!('unsafeWindow' in window))
         return;
 
-      let startTime = new URLSearchParams(location.search).get('t') || 0;
+    const _JSONParse = JSON.parse;
 
-      if (isNaN(startTime)) {
-        const multipliers = { h: 3600, m: 60, s: 1 };
-        startTime = startTime.match(/[0-9]+[a-z]/g).map(str => str.slice(0, -1) * multipliers[str.slice(-1)]).reduce((a, b) => a + b);
-      }
+    /**
+     * Since we need to change player information more than once, caching is effective.
+     */
+    const responsesCache = {};
 
-      const $iframe = $('<iframe>', {
-        src: `https://www.youtube.com/embed/${getVideoId()}?start=${startTime}&autoplay=1`,
-        id: 'bypassed-player',
-        style: 'border:0;width:100%;height:100%',
-        allowfullscreen: 1
-      });
-
-      $errorScreen.hide();
-      $('#ytd-player').hide();
-      $iframe.appendTo('#player-container');
-    });
-  }
-
-  function bypassEmbed() {
-    let ytcfg;
-
-    const _XMLHttpRequest = XMLHttpRequest
-
-    Object.defineProperty(unsafeWindow, 'ytcfg', {
-      set(value) {
-        ytcfg = value;
-
-        const orgSetter = ytcfg.set;
-
-        ytcfg.set = (...args) => {
-          try {
-            if (JSON.parse(args[0].PLAYER_VARS.embedded_player_response).playabilityStatus.status === 'UNPLAYABLE') {
-              const videoId = getVideoId();
-
-              const xhr = new _XMLHttpRequest();
-              xhr.open('GET', 'https://www.youtube.com/get_video_info?asv=3&video_id=' + videoId + '&eurl=https://youtube.googleapis.com/v/' + videoId, false);
-              xhr.send();
-
-              const response = new URLSearchParams(xhr.response).get('player_response');
-
-              if (JSON.parse(response).playabilityStatus.status === 'OK')
-                args[0].PLAYER_VARS.embedded_player_response = response;
-            }
-          }
-          catch (e) {
-            console.error(e);
-          }
-
-          orgSetter(...args)
-        }
-      },
-      get() {
-        return ytcfg;
-      }
-    });
-
-    unsafeWindow.XMLHttpRequest = class extends _XMLHttpRequest {
-      open(...args) {
-        this.url = args[1];
-        super.open(...args);
-      }
-
-      get response() {
-        const response = ytcfg.get('PLAYER_VARS').embedded_player_response;
-
-        if (this.url.includes('/youtubei/v1/player?key=') && response)
-          return response;
-        else
-          return super.response;
-      }
+    /**
+     * @param {string} status 
+     * @returns {boolean}
+     */
+    function isBypassable(status) {
+        return ['unplayable', 'age_verification_required', 'login_required'].includes(status.toLowerCase());
     }
-  }
 
-  init();
+    /**
+     * Get palyer response of restricted video
+     * 
+     * @param {string} videoId 
+     * @returns {boolean}
+     */
+    function bypassResponse(videoId) {
+        if (responsesCache[videoId] && responsesCache[videoId].time - Date.now() < 20 * 1000)
+            return responsesCache[videoId].data;
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://www.youtube.com/get_video_info?asv=3&video_id=' + videoId + '&eurl=https://youtube.googleapis.com/v/' + videoId, false);
+        xhr.send();
+
+        const response = JSON.parse(new URLSearchParams(xhr.response).get('player_response'));
+
+        if (response.playabilityStatus.status === 'OK') {
+            responsesCache[videoId] = {
+                time: Date.now(),
+                data: response
+            };
+            return response;
+        }
+        else
+            return false;
+    }
+
+    /**
+     * @param {object} obj 
+     * @returns {object}
+     */
+    function bypassObject(obj) {
+        const playabilityStatus = obj.playabilityStatus.status;
+
+        if (isBypassable(playabilityStatus)) {
+            const videoId = obj.videoDetails.videoId;
+            const bypassedResponse = bypassResponse(videoId);
+
+            if (bypassedResponse !== false)
+                return bypassedResponse;
+        }
+
+        throw ("Couldn't bypass");
+    }
+
+    function init() {
+        let bypassedInitialResponse = null;
+
+        const _defineProperty = Object.defineProperty;
+
+        /**
+         * When YouTube video is accesed directly, YT adds `ytInitialPlayerResponse` object to the page, which contains the video 
+         * information and restrictions. It is already JavaScript object, so intersecting `JSON.prepare` won't do anything. In order to bypass
+         * the information, it is necessary to define object property setter manually.
+         */
+        _defineProperty(unsafeWindow, "ytInitialPlayerResponse", {
+            configurable: true,
+            set: function (value) {
+                try {
+                    bypassedInitialResponse = bypassObject(value);
+                }
+                catch {
+                    bypassedInitialResponse = value;
+                }
+            },
+            get: function () {
+                return bypassedInitialResponse;
+            }
+        });
+
+        /**
+         * AdBlock and other extensions tends to change `ytInitialPlayerResponse` setter aswell, thus overwriting our changes.
+         */
+        unsafeWindow.Object.defineProperty = function (obj, prop, descriptor) {
+            if (obj === unsafeWindow && prop === "ytInitialPlayerResponse") {
+                console.info("Blocked redefining 'ytInitialPlayerResponse', sorry AdBlock :(");
+            } else {
+                _defineProperty(obj, prop, descriptor);
+            }
+        }
+
+        /**
+         * It is necessary to intersect `JSON.parse` method, since it is the only method which receives final data before they are
+         * processed. Intersecting `XMLHttpRequest` might seem like a good idea, hovewer YouTube combines many responses into the final
+         * JSON so we might not receive final data.
+         */
+        unsafeWindow.JSON.parse = function (text, reviver) {
+            let dataObj = _JSONParse(text, reviver);
+
+            try {
+                if ('playerResponse' in dataObj)
+                    dataObj.playerResponse = bypassObject(dataObj.playerResponse);
+                else if ('playabilityStatus' in dataObj)
+                    dataObj = bypassObject(dataObj);
+                else if (Array.isArray(dataObj)) {
+                    const responseElement = dataObj.find(o => 'playerResponse' in o);
+
+                    if (responseElement)
+                        responseElement.playerResponse = bypassObject(responseElement.playerResponse);
+                }
+            } catch { }
+
+            return dataObj;
+        }
+    }
+
+    init();
 })();
